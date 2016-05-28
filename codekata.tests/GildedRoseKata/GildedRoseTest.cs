7cc8b5292a0e0a4a2868bca32666d91a25c25503
Amar Kumar Dubedy <amdubedy@microsoft.com>
2016-05-28 16:28:52 -0700

    Setup for GildedRose Kata done

---
+++ codekata.tests/GildedRoseKata/GildedRoseTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using codekata.GildedRoseKata;
+using System.Text;
+using System.IO;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+
+namespace codekata.tests
+{
+    [TestClass]
+    public class GildedRoseTest
+    {
+        [TestMethod]
+        public void foo()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("foo", Items[0].Name);
+        }
+
+        [UseReporter(typeof(DiffReporter))]
+        [TestMethod]
+        public void ThirtyDays()
+        {
+            StringBuilder fakeoutput = new StringBuilder();
+            Console.SetOut(new StringWriter(fakeoutput));
+            Console.SetIn(new StringReader("a\n"));
+
+            TestTextFixture.TestGildedRose();
+            String output = fakeoutput.ToString();
+            Approvals.Verify(output);
+        }
+    }
+}
